9c5177bd764595a6e978dc4bdf54699af0cb0a15
david20045 <david.ovanesyan.44@icloud.com>
2025-08-01 16:34:15 +0400

    Обновил билд и настройки сервера3

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 // При всех остальных запросах отдаем index.html (для SPA)
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
