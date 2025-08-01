ce195c18ccc0da76337e9825be33d69aecea5e0d
david20045 <david.ovanesyan.44@icloud.com>
2025-08-01 16:54:58 +0400

    Обновил билд и настройки сервера5

--- server/server.js
+++ server/server.js
@@ -21,6 +21,11 @@ const io = new Server(server, { cors: { origin: "*" } });
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log("Route:", req.method, req.url);
+  next();
+});
+
 // Отдача статических файлов фронтенда из папки public
 app.use(express.static(path.join(__dirname, "public")));
 
